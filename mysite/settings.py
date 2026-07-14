326b6ff66c387d971e57573a66cc7ff176ffab1e
Your Name <your.email@example.com>
2026-07-14 15:28:10 +0500

    Configure Django project for Vercel deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--##r&h16++jd$=x1s$9(st&8i19c$u^2x9xcc5+y)szg2by(ve
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
